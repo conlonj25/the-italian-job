1407b9330671d05f6688c927c2e3f2320cd678b6
James Conlon <conlonj25@gmail.com>
2025-03-21 20:34:05 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
+	// "github.com/hajimehoshi/ebiten/v2/text"
+)
+
+const (
+	screenWidth      = 640
+	screenHeight     = 480
+	tileSize         = 32
+	titleFontSize    = fontSize * 1.5
+	fontSize         = 24
+	smallFontSize    = fontSize / 2
+	pipeWidth        = tileSize * 2
+	pipeStartOffsetX = 8
+	pipeIntervalX    = 8
+	pipeGapY         = 5
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.RGBA{ 0x00, 0xbc, 0xff, 0xff })
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Flappy")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
