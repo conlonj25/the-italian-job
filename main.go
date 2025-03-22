6f4f11f05c4953fbeac998182b770e2ce400cb07
James Conlon <conlonj25@gmail.com>
2025-03-22 15:07:44 +0100

    get floor moving

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"log"
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"bytes"
+	"fmt"
+	"image"
 	"image/color"
-	// "github.com/hajimehoshi/ebiten/v2/text"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	resources "github.com/hajimehoshi/ebiten/v2/examples/resources/images/flappy"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
+
+func floorDiv(x, y int) int {
+	d := x / y
+	if d*y == x || x >= 0 {
+		return d
+	}
+	return d - 1
+}
+
+func floorMod(x, y int) int {
+	return x - floorDiv(x, y)*y
+}
 
 const (
 	screenWidth      = 640
@@ -21,23 +39,111 @@ const (
 	pipeGapY         = 5
 )
 
-type Game struct{}
+var (
+	gopherImage      *ebiten.Image
+	tilesImage       *ebiten.Image
+	arcadeFaceSource *text.GoTextFaceSource
+)
+
+func init() {
+	img, _, err := image.Decode(bytes.NewReader(resources.Gopher_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+	gopherImage = ebiten.NewImageFromImage(img)
+
+	img, _, err = image.Decode(bytes.NewReader(resources.Tiles_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+	tilesImage = ebiten.NewImageFromImage(img)
+}
+
+func init() {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.PressStart2P_ttf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	arcadeFaceSource = s
+}
+
+type Game struct {
+	mode  int
+	score int
+
+	// The gopher's position
+	x16  int
+	y16  int
+	vy16 int
+
+	// Camera
+	cameraX int
+	cameraY int
+
+	// Pipes
+	pipeTileYs []int
+
+	gameoverCount int
+
+	touchIDs   []ebiten.TouchID
+	gamepadIDs []ebiten.GamepadID
+
+	audioContext *audio.Context
+	jumpPlayer   *audio.Player
+	hitPlayer    *audio.Player
+}
 
 func (g *Game) Update() error {
+	g.cameraX += 2
+
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-	screen.Fill(color.RGBA{ 0x00, 0xbc, 0xff, 0xff })
+func (g *Game) DrawTiles(screen *ebiten.Image) {
+	const (
+		nx           = screenWidth / tileSize
+		ny           = screenHeight / tileSize
+		pipeTileSrcX = 128
+		pipeTileSrcY = 192
+	)
+
+	op := &ebiten.DrawImageOptions{}
+
+	// g.DrawTiles(screen)
+	op.GeoM.Reset()
+	for i := -2; i < nx+1; i++ {
+		// ground
+		op.GeoM.Reset()
+		op.GeoM.Translate(float64(i*tileSize-floorMod(g.cameraX, tileSize)),
+			float64((ny-1)*tileSize-floorMod(g.cameraY, tileSize)))
+		screen.DrawImage(tilesImage.SubImage(image.Rect(0, 0, tileSize, tileSize)).(*ebiten.Image), op)
+	}
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0x00, 0xbc, 0xff, 0xff})
+
+	op := &text.DrawOptions{}
+
+	g.DrawTiles(screen)
+
+	op = &text.DrawOptions{}
+	op.GeoM.Translate(screenWidth, 0)
+	op.ColorScale.ScaleWithColor(color.White)
+	op.LineSpacing = fontSize
+	op.PrimaryAlign = text.AlignEnd
+	text.Draw(screen, fmt.Sprintf("%04d", g.score), &text.GoTextFace{
+		Source: arcadeFaceSource,
+		Size:   fontSize,
+	}, op)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Flappy")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
